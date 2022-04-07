a83116b8e1b448e1413cbcc372c447a0ab00d2c5
PashkaVRN <103056148+PashkaVRN@users.noreply.github.com>
2022-04-07 21:16:43 +0300

    Initial commit

---
+++ homework.py
@@ -0,0 +1,68 @@
+class InfoMessage:
+    """Информационное сообщение о тренировке."""
+    pass
+
+
+class Training:
+    """Базовый класс тренировки."""
+
+    def __init__(self,
+                 action: int,
+                 duration: float,
+                 weight: float,
+                 ) -> None:
+        pass
+
+    def get_distance(self) -> float:
+        """Получить дистанцию в км."""
+        pass
+
+    def get_mean_speed(self) -> float:
+        """Получить среднюю скорость движения."""
+        pass
+
+    def get_spent_calories(self) -> float:
+        """Получить количество затраченных калорий."""
+        pass
+
+    def show_training_info(self) -> InfoMessage:
+        """Вернуть информационное сообщение о выполненной тренировке."""
+        pass
+
+
+class Running(Training):
+    """Тренировка: бег."""
+    pass
+
+
+class SportsWalking(Training):
+    """Тренировка: спортивная ходьба."""
+    pass
+
+
+class Swimming(Training):
+    """Тренировка: плавание."""
+    pass
+
+
+def read_package(workout_type: str, data: list) -> Training:
+    """Прочитать данные полученные от датчиков."""
+    pass
+
+
+def main(training: Training) -> None:
+    """Главная функция."""
+    pass
+
+
+if __name__ == '__main__':
+    packages = [
+        ('SWM', [720, 1, 80, 25, 40]),
+        ('RUN', [15000, 1, 75]),
+        ('WLK', [9000, 1, 75, 180]),
+    ]
+
+    for workout_type, data in packages:
+        training = read_package(workout_type, data)
+        main(training)
+
